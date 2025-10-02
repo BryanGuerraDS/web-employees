b7a91a9bcf257a75cd905c6e75bd69b2909cb152
Bryan Danilo Guerra Suarez <bryan.guerra@utec.edu.pe>
2025-10-01 22:00:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var enlace_api = 'http://18.210.225.185:8000/'
+var enlace_api = 'http://18.210.225.185:8000'
 
 var solicitar_lista=  (event) => {
         fetch(enlace_api + '/employees')
@@ -49,4 +49,5 @@ if(document.forms[0]){
             console.log(err);
         })  
     });
+
 }
